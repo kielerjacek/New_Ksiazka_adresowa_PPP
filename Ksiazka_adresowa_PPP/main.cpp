5df91bb46467c377775db7a001164652ede6f81e
kielerjacek <kielerjacek.programista@gmail.com>
2018-02-26 20:40:58 +0100

    Initial Commit

---
+++ Ksiazka_adresowa_PPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ksiazka_adresowa_PPP
+//
+//  Created by Jacek Kieler on 26.02.2018.
+//  Copyright © 2018 Jacek Kieler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
